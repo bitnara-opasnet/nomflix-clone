704be184239f4529584f6af8934ad43156df2779
bitnara-opasnet <84299110+bitnara-opasnet@users.noreply.github.com>
2022-12-17 22:43:57 +0900

    12-17 nomflix-clone start

---
+++ README.txt
@@ -0,0 +1,12 @@
+npm i --save-dev @types/styled-components
+npm i styled-components
+
+npm i react-router-dom@5.3.0
+npm i react-query
+npm i --save-dev @types/react-router-dom
+
+npm i react-helmet
+npm i react-helmet-async
+npm i --save-dev @types/react-helmet
+npm i recoil
+npm i framer-motion
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+import Header from "./Components/Headers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <Header />
+            <Switch>
+                <Route path="/">
+                    <Home />
+                </Route>
+                <Route path="/tv">
+                    <Tv />
+                </Route>
+                <Route path="/search">
+                    <Search />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
-
-export default App;
