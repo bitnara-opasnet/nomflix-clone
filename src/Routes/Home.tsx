6b3ef3cadafddc6f07b78928cc0a2612d1b2deeb
bitnara <bitnara@opasnet.co.kr>
2023-01-04 15:01:05 +0900

    01-04 slider, box animation

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,11 +1,14 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
+import { useState } from "react";
 
 
 const Wrapper = styled.div`
     background-color: black;
+    padding-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -21,37 +24,166 @@ const Banner = styled.div<{bgPhoto:string}>`
     flex-direction: column;
     justify-content: center;
     padding: 60px;
-    background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${(props) => props.bgPhoto});
+    background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1)), url(${(props) => props.bgPhoto});
     background-size: cover;
 `;
 
 const Title = styled.h2`
-    font-size: 50px;
+    font-size: 68px;
     margin-bottom: 20px;
 `;
 
 const OverView = styled.p`
-    font-size: 20px;
+    font-size: 30px;
     width: 50%;
 `;
 
+const Slider = styled.div`
+    position: relative;
+    top: -100px;
+`;
 
+const Row = styled(motion.div)`
+    display: grid;
+    gap: 5px;
+    grid-template-columns: repeat(6, 1fr);
+    position: absolute;
+    width: 100%;
+`;
+
+const Box = styled(motion.div)<{bgPhoto: string}>`
+    height: 200px;
+    background-color: white;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
+
+    &:first-child {
+        transform-origin: center left;
+    };
+    &:last-child {
+        transform-origin: center right;
+    };
+`;
+
+
+const Info = styled(motion.div)`
+    padding: 10px;
+    background-color: ${(props) => props.theme.black.lighter};
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    h4 {
+        text-align: center;
+        font-size: 18px;
+    }
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth + 5,
+        transition: { 
+            type: "tween", 
+            duration: 1 
+    }
+    },
+    visible: {
+        x: 0,
+    },
+    exit: {
+        x: -window.outerWidth - 5,
+    },
+};
+
+const boxVariants = {
+    normal: {
+        scale: 1,
+        transition: {
+            type: "tween",
+        }
+    },
+    hover: {
+        scale: 1.3,
+        y: -80,
+        transition: {
+            delay: 0.5,
+            duaration: 0.1,
+            type: "tween",
+        },
+    },
+};
+
+const infoVariants = {
+    hover: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duaration: 0.1,
+            type: "tween",
+        },
+    }
+};
+
+const offset = 6;
 
 function Home() {
     const {data, isLoading} = useQuery<IGetMoviesResult>(
         ["movies", "nowPlaying"], 
         getMovies
     );
-
+    const [index, setIndex] = useState(0);
+    const [leaving, setLeaving] = useState(false);
+    const incraseIndex = () => {
+        if (data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalMovies = data.results.length -1;
+            const MaxIndex = Math.floor(totalMovies / offset);
+            setIndex((prev) => prev === MaxIndex ? 0 : prev + 1);
+        };
+    };
+    const toggleLeaving = () => setLeaving((prev) => !prev);
     return (
         <Wrapper>
             {isLoading ? (<Loader>Loading..</Loader> 
             ): (
                 <>
-                    <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                    <Banner 
+                        bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                        onClick={incraseIndex}
+                    >
                         <Title>{data?.results[0].title}</Title>
                         <OverView>{data?.results[0].overview}</OverView>
                     </Banner>
+                    <Slider>
+                        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                            <Row
+                                key={index}
+                                variants={rowVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                transition={{ type: "tween", duration: 1 }}
+                            >
+                                {data?.results.slice(1).slice(offset*index, offset*index+offset).map((movie) => (
+                                    <Box key={movie.id}
+                                        bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                        variants={boxVariants}
+                                        whileHover="hover"
+                                        initial="normal"
+                                    >
+                                        <Info
+                                            variants={infoVariants}
+                                        >
+                                            <h4>{movie.title}</h4>
+                                        </Info>
+                                    </Box>
+                                ))}
+                                {/* {[1, 2, 3, 4, 5, 6].map((i) => (<Box key={i}>{i}</Box>))} */}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
                 </>
             )}
         </Wrapper>
